ebefdfb4a2c7e893f6fc4b6db1253fb263da8ae5
Odhiambo Dormnic <d.odhiambo@carepay.co.ke>
2019-12-16 15:21:12 +0300

    [Task] -> Setup a Django Project

---
+++ wfp/main/helpers.py
@@ -0,0 +1,23 @@
+import json
+
+from django.conf import settings;
+
+AVAILABLE_REGIONS = [
+    ('Global', 'Global'),
+    ('Africa', 'Africa'),
+    ('Americas', 'Americas'),
+    ('South-East Asia', 'South-East Asia'),
+    ('Europe', 'Europe'),
+    ('Eastern Mediterranean', 'Eastern Mediterranean'),
+    ('Western Pacific', 'Western Pacific')
+]
+
+GENDERS = [
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Both sexes', 'Both sexes')
+]
+
+file = open(settings.DATA_FILE, 'r')
+
+DEFAULT = json.loads(file.read())

7b820596e26c258ca5d629d53e4ddfa7ed59f41c
Odhiambo Dormnic <d.odhiambo@carepay.co.ke>
2019-12-18 12:20:22 +0300

    new changes

--- wfp/wfp/settings.py
+++ wfp/wfp/settings.py
@@ -25,7 +25,17 @@ SECRET_KEY = '6*gn_ikp5_-z(l^$m2_re!eanerhdjg*5wo3qp3g$x@_-pdvgj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ngrok.io', 'localhost', '127.0.0.1']
+
+CORS_ORIGIN_ALLOW_AL = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # Application definition
 
@@ -43,6 +53,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 
 ]
 
